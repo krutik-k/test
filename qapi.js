48a6244158e530c0993a7821adb3290f4490eafb
krutik-k <68330578+krutik-k@users.noreply.github.com>
2020-11-26 20:53:16 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fbfc7cfb83a2f001f2e3c18",
+				"5fbfc7cfb83a2f001f2e3c1a"
+			]
+		},
+		"5fbfc7cfb83a2f001f2e3c18": {
+			"id": "5fbfc7cfb83a2f001f2e3c18",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fbfc7cfb83a2f001f2e3c1a": {
+			"id": "5fbfc7cfb83a2f001f2e3c1a",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
